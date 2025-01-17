d4ec7f0e0ac7d55f06ac9d0f58c10f124a84e53a
Alan Carrizo <alancarrizo1017@gmail.com>
2025-01-17 20:55:38 -0300

    Estructura básica e inicio de Portfolio personal utilizando React + Vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './styles.scss'
+import Navbar from "./components/navbar.jsx"
+import Portfolio from "./components/portfolio.jsx"
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Portfolio/>
+    </>
+  )
+}
+
+export default App
