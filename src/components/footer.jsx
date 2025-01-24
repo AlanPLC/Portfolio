aa2527c29fa746666ecbc165cc627e23261f848a
Alan Carrizo <alancarrizo1017@gmail.com>
2025-01-24 18:20:57 -0300

    Agregado de atribuciones íconos PNG en footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,33 @@ export default function Footer() {
 
     return(
         <footer>
-            <p>© 2025 Carrizo Alan Pablo Lautaro. Todos los derechos reservados.</p>
+            <p className='poppins-regular'>© 2025 Carrizo Alan Pablo Lautaro. Todos los derechos reservados.</p>
+
+            <div className='atribuciones'>
+                <p className='poppins-bold'>Atribuciones íconos Conocimientos</p>
+                <ul>
+                    <li>
+                        <img src="./Proyectos/code.svg" alt="code-icon" />
+                        <a href="https://www.flaticon.es/iconos-gratis/javascript" title="javascript iconos">Javascript iconos creados por Freepik - Flaticon</a>
+                    </li>
+                    <li>
+                        <img src="./Proyectos/code.svg" alt="code-icon" />
+                        <a href="https://www.flaticon.es/iconos-gratis/nodo-js" title="nodo js iconos">Nodo js iconos creados por Freepik - Flaticon</a>
+                    </li>
+                    <li>
+                        <img src="./Proyectos/code.svg" alt="code-icon" />
+                        <a href="https://www.flaticon.es/iconos-gratis/css" title="css iconos">Css iconos creados por Pixel perfect - Flaticon</a>
+                    </li>
+                    <li>
+                        <img src="./Proyectos/code.svg" alt="code-icon" />
+                        <a href="https://www.flaticon.es/iconos-gratis/html-5" title="html 5 iconos">Html 5 iconos creados por Pixel perfect - Flaticon</a>
+                    </li>
+                    <li>
+                      <img src="./Proyectos/code.svg" alt="code-icon" />
+                        <a href="https://www.flaticon.es/iconos-gratis/piton" title="pitón iconos">Pitón iconos creados por Freepik - Flaticon</a>
+                    </li>
+                </ul>
+            </div>
         </footer>
     )
 }
